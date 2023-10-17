8944d13359c00b538aab73f46c7f115c356afe51
konstantin-klymenko <72839214+konstantin-klymenko@users.noreply.github.com>
2023-10-17 16:13:29 +0300

    add homework #2

---
+++ Homework2 #1.py
@@ -0,0 +1,105 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+# Декоратор для обробки помилки ValueError
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+    return inner
+
+# Декоратор для обробки помилки KeyError
+def key_error_handler(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found."
+    return inner
+
+# Вітання користувача
+def hello():
+    return "How can I help you?"
+
+# Для додавання нового контакту
+@input_error
+def add_contact(contacts, name, phone):
+    contacts[name] = phone
+    return "Contact added."
+
+# Для зміни номера телефону у вже існуючого контакту
+@key_error_handler
+@input_error
+def change_contact(contacts, name, new_phone):
+    contacts[name] = new_phone
+    return "Contact updated."
+
+# Для виведення номера телефону за ім'ям
+@key_error_handler
+def show_phone(contacts, name):
+    return contacts[name]
+
+def show_all(contacts):
+    return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+
+# Головна функція, яка керує взаємодією з користувачем
+def main():
+    # Створюємо словник для збереження контактів
+    contacts = {}
+    print("Welcome to the assistant bot!")
+
+    while True:
+        # Користувач вводить команду
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        # Перевіряємо, чи користувач не хоче завершити роботу бота
+        if command in ["close", "exit"]:
+            print("Goodbye!")
+            break
+
+        # Обробляємо команди
+        if command == "hello":
+            print(hello())
+
+        elif command == "add":
+            if len(args) == 2:
+                name, phone = args
+                print(add_contact(contacts, name, phone))
+            else:
+                print("Invalid command: Give me name and phone please.")
+
+        elif command == "change":
+            if len(args) == 2:
+                name, new_phone = args
+                print(change_contact(contacts, name, new_phone))
+            else:
+                print("Invalid command: Give me name and phone please.")
+
+        elif command == "phone":
+            if len(args) == 1:
+                name = args[0]
+                result = show_phone(contacts, name)
+                if result == "Contact not found.":
+                    print("Contact not found.")
+                else:
+                    print(result)
+            else:
+                print("Invalid command.")
+
+        elif command == "all":
+            result = show_all(contacts)
+            if result:
+                print(result)
+            else:
+                print("No contacts in the list.")
+
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
---
+++ Homework2 #2.py
@@ -0,0 +1,98 @@
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        if len(value) < 1:
+            raise ValueError("Name can't be empty")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be a 10-digit number")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        try:
+            phone = Phone(phone)
+            self.phones.append(phone)
+        except ValueError as e:
+            print(e)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+        return None
+
+    def __str__(self):
+        phone_str = "; ".join(str(p) for p in self.phones)
+        return f"Contact name: {self.name}, phones: {phone_str}"
+
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+
+john = book.find("John")
+jane = book.find("Jane")
+john.edit_phone("1234567890", "1112223333")
+jane.edit_phone("1111111111", "1112223333")
+
+print(john)  
+
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  
+
+
+# Видалення  "Jane"
+book.delete("Jane")
+
